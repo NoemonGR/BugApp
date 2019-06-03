fe51d5274558b5e7d36a928780ca2575a2a4a353
Stylianos Theodorikakos <stelios-theodorikakos@hotmail.com>
2019-06-03 07:24:35 +0300

    status report added

---
+++ src/app/mainFolder/story2/story2/reporter.model.ts
@@ -0,0 +1,4 @@
+export class Reporter {
+    id: number;
+    name: string;
+}
--- src/app/mainFolder/story2/story2/story2.component.ts
+++ src/app/mainFolder/story2/story2/story2.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; 
 import { Priority } from "src/app/mainFolder/story2/story2/priority.model";
+import { Reporter } from "src/app/mainFolder/story2/story2/reporter.model";
+import { Status } from './status.model';
 
 @Component({
   selector: 'app-story2',
@@ -10,10 +12,24 @@ import { Priority } from "src/app/mainFolder/story2/story2/priority.model";
 export class Story2Component implements OnInit {
 
   priorities: Priority[] = [
+    {id: 1, name: 'Minor'},
+    {id: 2, name: 'Major'},
+    {id: 3, name: 'Critical'}
+  ];
+
+  reporters: Reporter[] = [
     {id: 1, name: 'QA'},
     {id: 2, name: 'PO'},
     {id: 3, name: 'DEV'}
   ];
+
+  statuses: Status[] = [
+    {id: 1, name: 'Ready for test'},
+    {id: 2, name: 'Done'},
+    {id: 3, name: 'Rejected'}
+  ];
+
+
   constructor(private router: Router) { }
 
   ngOnInit() {
