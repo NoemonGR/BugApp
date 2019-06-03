a5791f66a5414a6244a17e6d6679b00ade9caa02
Stylianos Theodorikakos <stelios-theodorikakos@hotmail.com>
2019-06-03 18:25:05 +0300

    getbugbyid in service story 2

--- src/app/mainFolder/story2/story2.service.ts
+++ src/app/mainFolder/story2/story2.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { bug } from 'src/app/mainFolder/models/story1.model';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -12,6 +13,10 @@ export class Story2Service {
 
   createBugs(newB: bug) {
     this.http.post<bug[]>('https://bug-report-system-server.herokuapp.com/bugs', newB);
+  }
+
+  getBugById(id): Observable<bug> {
+      return this.http.get<bug>('https://bug-report-system-server.herokuapp.com/bugs?sort=reporter,asc' + '/' + id);
   }
 
 }
