aa7ac652f0f212a1def7e41fbfc6f80a3f3f32e2
Stelios Theodorikakos <stelios-theodorikakos@hotmail.com>
2019-06-23 00:02:39 +0300

    ts-lint

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,36 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AboutComponent } from './about.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { Router } from '@angular/router';
+import {AboutComponent} from './about.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Router} from '@angular/router';
 
-// den 3erw giati moy emfanizei autes tis malakies
+// npm
 
 describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+    let component: AboutComponent;
+    let fixture: ComponentFixture<AboutComponent>;
 
+    beforeEach(async (() => {
+        TestBed
+            .configureTestingModule(
+                {imports: [RouterTestingModule], declarations: [AboutComponent], providers: []}
+            )
+            .compileComponents();
+    }));
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [ AboutComponent
-      ],
-      providers: [
-      ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AboutComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
